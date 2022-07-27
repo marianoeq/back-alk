913033a54629cf117ccf5a80728f29274be5777f
marian.eq <mariano.eq@gmail.com>
2022-07-27 21:29:43 +0100

    test

--- server/config.js
+++ server/config.js
@@ -2,5 +2,7 @@ export const DATABASE_HOST = process.env.DB_HOST;
 export const DATABASE_NAME = process.env.DB_NAME;
 export const USERNAME = process.env.DB_USER;
 export const DB_PORT = process.env.DB_PORT;
-export const PASSWORD = process.env.DB_PASS;
+export const PASSWORD = process.env.DB_PASSWORD;
 export const URI = process.env.DB_URL;
+
+
